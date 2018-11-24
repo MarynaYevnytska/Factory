214093d925b051b6a4d248870ad66a0326c1206e
Yevnytska Maryna <me_home@ukr.net>
2018-11-24 20:03:56 +0200

    changes add ++

--- lib/factory.rb
+++ lib/factory.rb
@@ -3,7 +3,6 @@
 # * Arguments of creatable Factory instance are fields/attributes of created class
 # * The ability to add some methods to this class must be provided while creating a Factory
 # * We must have an ability to get/set the value of attribute like [0], ['attribute_name'], [:attribute_name]
-#
 # * Instance of creatable Factory class should correctly respond to main methods of Struct
 # - each
 # - each_pair
